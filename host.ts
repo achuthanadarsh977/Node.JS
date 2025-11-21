86f76cc73c7af03a2c8228686e6812f6414c406c
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-11-21 19:31:54 +0530

    Create host.ts

---
+++ host.ts
@@ -0,0 +1,12 @@
+
+const os = require('os')
+
+console.log("Free memory:"+os.freemem())
+
+console.log("Home Directory:"+os.homedir())
+
+console.log("Host Name:"+os.hostname())
+console.log("Release:"+os.release())
+console.log("Platform:"+os.platform())
+
+console.log("Load Avg:"+os.loadavg())
