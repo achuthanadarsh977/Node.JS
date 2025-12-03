ae0d433c9fc4200e98bdbfe34e2e18684c692f92
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-12-03 13:16:02 +0530

    Create movie.js

---
+++ Build Movie API/movie.js
@@ -0,0 +1,32 @@
+const { Movie, validate } = require('./movies');
+const { Genre } = require('./genres');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const movies = await Movie.find().sort('title');
+    res.send(movies);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send("Invalid genre.");
+
+    let movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalState: req.body.dailyRentalState
+    });
+
+    movie = await movie.save();
+    res.send(movie);
+});
+
+module.exports = router;
