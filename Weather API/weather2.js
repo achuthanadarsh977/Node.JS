34d120c902b1f3e75a60600c66053ddaec1dddba
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-12-13 19:34:25 +0530

    Update weather2.js

--- Weather API/weather2.js
+++ Weather API/weather2.js
@@ -1,16 +1,25 @@
-const weather1 = require("./weather1");
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const weatherRouter = require("./weather1");
+
 
 const app = express();
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+// Connect MongoDB
 mongoose
   .connect("mongodb://127.0.0.1:27017/weather")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to mongodb...", err));
 
-app.use("/api/weather", weather1);
+// API routes
+app.use("/api/weather", weatherRouter);
+
+// Serve frontend
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "weather.html"));
+});
 
 const PORT = 5000;
 app.listen(PORT, () =>
