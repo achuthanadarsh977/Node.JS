9db4b73ffb7339a3ee45d488596e9eea6697e508
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-01-12 19:17:21 +0530

    Add files via upload

---
+++ Logistics Management System/customercontroller.js
@@ -0,0 +1,11 @@
+
+exports.addCustomer  = async (req,res) => {
+    
+    const locals = {
+        title:"Add Customer - Node.JS",
+        description:"Free Node JS Logistics Management System"
+
+    }
+
+    res.render('logistics/add' , )
+}
